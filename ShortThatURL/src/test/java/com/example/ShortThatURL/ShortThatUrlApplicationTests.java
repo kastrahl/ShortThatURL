31175ad884866f7cc402455b45d786707fed838d
kastrahl <kushagraarora123@gmail.com>
2025-02-18 04:01:12 +0530

    initial spring initializer boiler

---
+++ ShortThatURL/src/test/java/com/example/ShortThatURL/ShortThatUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShortThatURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortThatUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
